c7b4e76697f8f90e10d17c1c267bdec5ec0f8af1
TJ Singleton <tjsingleton@vantagestreet.com>
2010-05-18 11:49:43 -0400

    basic approval

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,18 @@
 class Admin::UsersController < ApplicationController
   filter_access_to :all, :require => :read
-  
+
   def index
     @users = User.paginate :page => params[:page], :order => 'updated_at DESC'
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:notice] = 'Successfully updated user.'
+      redirect_to admin_users_path
+    else
+      flash[:error] = 'Failed to update user.'
+      render :action => 'edit'
+    end
   end
 end
