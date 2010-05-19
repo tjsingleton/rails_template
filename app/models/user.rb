5c4130c00ffb0ff1266b95dc1ba465a99b3984c8
David Sommers <databyte@gmail.com>
2010-05-19 14:03:38 -0400

    space

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
   include User::Role
   self.per_page = 10
 
-
   acts_as_authentic do |c|
     c.login_field = 'email'
     c.crypto_provider = Authlogic::CryptoProviders::BCrypt
